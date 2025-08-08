2dbf4db30ec39359396c4b357064530c4b7136b8
S@dbit <90182143+heeeee0000@users.noreply.github.com>
2025-08-08 08:03:34 -0700

    Create Sorting(Decending).cpp

---
+++ Sorting(Decending).cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cout<<"Enter the array size : ";
+    cin>>n;
+    int a[n];
+    for(int i = 0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n,greater<int>());
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    
+    
+}
